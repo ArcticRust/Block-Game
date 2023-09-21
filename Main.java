c8494972fb5215636dd66662f0ac5150bc8c2e8b
ArcticRust <118741608+ArcticRust@users.noreply.github.com>
2023-09-21 17:40:25 +0000

    state controlller worked on

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 class Main {
   public static void main(String[] args) {
     StateController stateController = new StateController();
-    Board board = new Board();
-    board.printDurability();
-    board.printOwnership();
-    Deck deck = new Deck();
-    deck.getRandomCard().printCard();
-    System.out.println(stateController.requestInput());
+    while (true) {
+      stateController.run();
+      if (stateController.gameOver) {
+        break;
+      }
+    }
   }
 }
--- StateController.java
+++ StateController.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 enum State {
     ViewingDurability,
@@ -6,16 +7,111 @@ enum State {
     PlacingCards,
     ViewingCards,
     AddingToCard,
+    ChoosingOption,
 }
 
 public class StateController {
-    int turn = 0;
-    int playerTurn = 1;
-    State currState;
-    
-    String requestInput() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            return scanner.nextLine();
+    public int turn = 0;
+    public int playerTurn = 1;
+    public boolean gameOver = false;
+    Board board = new Board();
+    private State currState = State.ChoosingOption;
+    private ArrayList<Card> player1Cards = new ArrayList<>();
+    private ArrayList<Card> player2Cards = new ArrayList<>();
+    Deck deck = new Deck();
+    Scanner scanner = new Scanner(System.in);
+    String resultString = "";
+    String white = "\u001B[37m";
+
+    public void run() {
+        clearTerminal();
+        System.out.print(resultString);
+        resultString = "";
+        switch (currState) {   
+
+            case ChoosingOption:
+                ChoosingOptionFunc();
+                break;
+            case ViewingDurability:
+                ViewingDurabilityFunc();
+                break;
+            case PlacingCards:
+                break;
+            case ViewingCards: 
+                break;
+            case AddingToCard:
+                break;
         }
+    }
+
+
+    void endTurn() {
+        turn++;
+    }
+
+    void giveCard(int player, Card card) {
+        switch (player) {
+            case 1:
+                player1Cards.add(card);
+                break;
+            case 2:
+                player2Cards.add(card);
+                break;
+            default:
+                System.out.println("this is broken, check inputs for giveCard function");
+        }
+    } 
+
+    String requestInput() {
+        return scanner.nextLine();
+    }
+
+    void clearTerminal() {
+        System.out.print("\033[H\033[2J");
+    }
+
+    void ViewingDurabilityFunc() {
+        board.printDurability();
+        System.out.println("Press q to view Durability\nPress w to go Back to Options");
+        switch (requestInput()) {
+            case "q":
+                currState = State.ViewingDurability;
+                break;
+            case "w":
+                currState = State.ChoosingOption;
+                break;
+            default:
+                resultString = "Please Choose a Valid Input";
+                break;
+        }
+    }
+
+    void ChoosingOptionFunc() {
+        System.out.println("Choose an action:\n q - View Durability of Board\n a - View Board Ownership\n w - Place a Card\n s - Add to a Card\n e - View your Cards");
+        String input = requestInput();
+        switch (input) {
+            case "q":
+                currState = State.ViewingDurability;
+                break;
+            case "a":
+                currState = State.ViewingOwnership;
+                break;
+            case "w":
+                currState = State.PlacingCards;
+                break;
+            case "s":
+                currState = State.AddingToCard;
+                break;
+            case "e":
+                currState = State.ViewingCards;
+                break;
+            default:
+                resultString = "Please Choose a Valid Input\n";
+                break;
+        }
+    }
+
+    void print(String val) {
+        System.out.println(white + val);
     }
 }
