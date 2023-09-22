1379a6aa26ee9e11e734288d74be452cca898954
ArcticRust <118741608+ArcticRust@users.noreply.github.com>
2023-09-22 04:10:48 +0000

    changed how states work

--- StateController.java
+++ StateController.java
@@ -1,47 +1,79 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-enum State {
-    ViewingDurability,
-    ViewingOwnership,
-    PlacingCards,
-    ViewingCards,
-    AddingToCard,
-    ChoosingOption,
-}
+
 
 public class StateController {
-    public int turn = 0;
-    public int playerTurn = 1;
-    public boolean gameOver = false;
-    Board board = new Board();
-    private State currState = State.ChoosingOption;
-    private ArrayList<Card> player1Cards = new ArrayList<>();
-    private ArrayList<Card> player2Cards = new ArrayList<>();
-    Deck deck = new Deck();
-    Scanner scanner = new Scanner(System.in);
-    String resultString = "";
-    String white = "\u001B[37m";
+    private enum State {
+        ViewingDurability {
+            @Override
+            public void action() {
+                board.printDurability();
+                println("Press q to view Durability\nPress w to go Back to Options");
+                switch (requestInput()) {
+                case "q":
+                    currState = State.ViewingDurability;
+                    break;
+                case "w":
+                    currState = State.ChoosingOption;
+                    break;
+                default:
+                    resultString = "Please Choose a Valid Input";
+                    break;
+                }
+            }
+        },
+        ViewingOwnership,
+        PlacingCards,
+        ViewingCards,
+        AddingToCard,
+        
+        ChoosingOption {
+            @Override
+            public void action() {
+                println("Choose an action:\n q - View Durability of Board\n a - View Board Ownership\n w - Place a Card\n s - Add to a Card\n e - View your Cards");
+                String input = requestInput();
+                switch (input) {
+                    case "q":
+                        currState = State.ViewingDurability;
+                        break;
+                    case "a":
+                        currState = State.ViewingOwnership;
+                        break;
+                    case "w":
+                        currState = State.PlacingCards;
+                        break;
+                    case "s":
+                        currState = State.AddingToCard;
+                        break;
+                    case "e":
+                        currState = State.ViewingCards;
+                        break;
+                    default:
+                        resultString = "Please Choose a Valid Input\n";
+                        break;
+                }
+            }
+        };
+
+        public void action() { println("not implemented yet"); };
+    }
+    private static int turn = 0;
+    private static int playerTurn = 1;
+    public static boolean gameOver = false;
+    private static Board board = new Board();
+    private static State currState = State.ChoosingOption;
+    private static ArrayList<Card> player1Cards = new ArrayList<>();
+    private static ArrayList<Card> player2Cards = new ArrayList<>();
+    private static Deck deck = new Deck();
+    private static Scanner scanner = new Scanner(System.in);
+    private static String resultString = "";
 
     public void run() {
         clearTerminal();
-        System.out.print(resultString);
+        print(resultString);
         resultString = "";
-        switch (currState) {   
-
-            case ChoosingOption:
-                ChoosingOptionFunc();
-                break;
-            case ViewingDurability:
-                ViewingDurabilityFunc();
-                break;
-            case PlacingCards:
-                break;
-            case ViewingCards: 
-                break;
-            case AddingToCard:
-                break;
-        }
+        currState.action();
     }
 
 
@@ -58,11 +90,11 @@ public class StateController {
                 player2Cards.add(card);
                 break;
             default:
-                System.out.println("this is broken, check inputs for giveCard function");
+                println("this is broken, check inputs for giveCard function");
         }
     } 
 
-    String requestInput() {
+    static String requestInput() {
         return scanner.nextLine();
     }
 
@@ -70,48 +102,12 @@ public class StateController {
         System.out.print("\033[H\033[2J");
     }
 
-    void ViewingDurabilityFunc() {
-        board.printDurability();
-        System.out.println("Press q to view Durability\nPress w to go Back to Options");
-        switch (requestInput()) {
-            case "q":
-                currState = State.ViewingDurability;
-                break;
-            case "w":
-                currState = State.ChoosingOption;
-                break;
-            default:
-                resultString = "Please Choose a Valid Input";
-                break;
-        }
-    }
 
-    void ChoosingOptionFunc() {
-        System.out.println("Choose an action:\n q - View Durability of Board\n a - View Board Ownership\n w - Place a Card\n s - Add to a Card\n e - View your Cards");
-        String input = requestInput();
-        switch (input) {
-            case "q":
-                currState = State.ViewingDurability;
-                break;
-            case "a":
-                currState = State.ViewingOwnership;
-                break;
-            case "w":
-                currState = State.PlacingCards;
-                break;
-            case "s":
-                currState = State.AddingToCard;
-                break;
-            case "e":
-                currState = State.ViewingCards;
-                break;
-            default:
-                resultString = "Please Choose a Valid Input\n";
-                break;
-        }
+    static void println(String val) {
+        System.out.println("\u001B[37m" + val);
     }
-
-    void print(String val) {
-        System.out.println(white + val);
+    
+    static void print(String val) {
+        System.out.print("\u001B[37m" + val);
     }
 }
